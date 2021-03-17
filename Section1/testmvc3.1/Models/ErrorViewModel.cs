2c695a1ff934f334a951d4d0783caf90a81f0a0a
mathieu rousseau <mr.rousseaumathieu@gmail.com>
2021-03-17 23:47:43 +0100

    premier commit + section 1 + section 2 -> OK

---
+++ Section1/testmvc3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testmvc3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
