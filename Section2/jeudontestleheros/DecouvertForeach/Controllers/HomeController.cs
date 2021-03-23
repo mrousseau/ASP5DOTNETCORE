6253017a1e8c6ca129828e890b6de95414bd2297
mathieu rousseau <mr.rousseaumathieu@gmail.com>
2021-03-23 23:22:10 +0100

    gestion viewbag + ctrl + @foreach

---
+++ Section2/jeudontestleheros/DecouvertForeach/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using DecouvertForeach.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecouvertForeach.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int[] monTableauInCtrl = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            this.ViewBag.TabInCtrl = monTableauInCtrl;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
