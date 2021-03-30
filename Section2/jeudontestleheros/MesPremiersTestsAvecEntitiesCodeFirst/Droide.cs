c97cf1af96845d5b9eb28f3da3de8e42e3f919eb
mathieu rousseau <mr.rousseaumathieu@gmail.com>
2021-03-30 23:35:04 +0200

    gestion + connection a la base de donnée + affichage

--- Section2/jeudontestleheros/MesPremiersTestsAvecEntitiesCodeFirst/Droide.cs
+++ Section2/jeudontestleheros/MesPremiersTestsAvecEntitiesCodeFirst/Droide.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MesPremiersTestsAvecEntitiesCodeFirst
 {
+    [Table("Droides")]
     public class Droide
     {
         public int Id { get; set; }
